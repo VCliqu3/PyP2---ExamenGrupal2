f9064865ef5cd1a350d797bf104aad66e3b7b8f1
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-10-24 03:17:47 -0500

    Reworked Node 2

--- Assets/Scripts/Nodes/Node.cs
+++ Assets/Scripts/Nodes/Node.cs
@@ -9,8 +9,16 @@ public class Node : MonoBehaviour
 
     [Header("Components")]
     [SerializeField] private List<NodePosition> structurePositions;
-    [SerializeField] private List<NodePosition> groundPositions;
-    [SerializeField] private List<NodePosition> aerealPositions;
+    [SerializeField] private List<NodePosition> groundAliedPositions;
+    [SerializeField] private List<NodePosition> aerealAliedPositions;
+    [Space]
+    [SerializeField] private List<NodePosition> groundEnemyPositions;
+    [SerializeField] private List<NodePosition> aerealEnemyPositions;
 
     public int NodeNumber => nodeNumber;
+    public List<NodePosition> StructurePositions => structurePositions;
+    public List<NodePosition> GroundAliedPositions => groundAliedPositions;
+    public List<NodePosition> AerealAliedPositions => aerealAliedPositions;
+    public List<NodePosition> GroundEnemyPositions => groundEnemyPositions;
+    public List<NodePosition> AerealEnemyPositions => aerealEnemyPositions;
 }
