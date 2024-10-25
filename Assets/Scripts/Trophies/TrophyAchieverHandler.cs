41d498cfd592145b846d86893aa9d7e7312b0fc6
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-10-25 15:36:49 -0500

    Trophies

--- Assets/Scripts/Trophies/TrophyAchieverHandler.cs
+++ Assets/Scripts/Trophies/TrophyAchieverHandler.cs
@@ -5,17 +5,86 @@ using UnityEngine;
 
 public class TrophyAchieverHandler : MonoBehaviour
 {
-    private const int MEAT_TROPHY_ID = 248167;
-    private const int FISH_TROPHY_ID = 248168;
-    private const int HARVEST_TROPHY_ID = 248169;
-    private const int MINERAL_TROPHY_ID = 248170;
-    private const int CITIZEN_TROPHY_ID = 248171;
+    private const int WAVE1_TROPHY_ID = 248424;
+    private const int WAVE2_TROPHY_ID = 248425;
+    private const int UNITS1_TROPHY_ID = 248427;
+    private const int UNITS2_TROPHY_ID = 248426;
+    private const int GOLD_TROPHY_ID = 248428;
 
-    private const int MEAT_TROPHY_QUANTITY = 100;
-    private const int FISH_TROPHY_QUANTITY = 100;
-    private const int HARVEST_TROPHY_QUANTITY = 100;
-    private const int MINERAL_TROPHY_QUANTITY = 100;
-    private const int CITIZEN_TROPHY_QUANTITY = 10;
+    private const int WAVE1 = 5;
+    private const int WAVE2 = 10;
+    private const int UNITS1 = 10;
+    private const int UNITS2 = 15;
+    private const int GOLD = 500;
 
-    
+    private void OnEnable()
+    {
+        EnemySpawnManager.OnWaveStartSpawn += EnemySpawnManager_OnWaveStartSpawn;
+        EntitiesManager.OnAnyAllyEntityAdded += EntitiesManager_OnAnyAllyEntityAdded;
+        GoldManager.OnGoldIncreased += GoldManager_OnGoldIncreased;
+    }
+    private void OnDisable()
+    {
+        EnemySpawnManager.OnWaveStartSpawn -= EnemySpawnManager_OnWaveStartSpawn;
+        EntitiesManager.OnAnyAllyEntityAdded -= EntitiesManager_OnAnyAllyEntityAdded;
+        GoldManager.OnGoldIncreased -= GoldManager_OnGoldIncreased;
+    }
+
+    private void Start()
+    {
+        Trophies.Remove(WAVE1_TROPHY_ID);
+        Trophies.Remove(WAVE2_TROPHY_ID);
+        Trophies.Remove(UNITS1_TROPHY_ID);
+        Trophies.Remove(UNITS2_TROPHY_ID);
+        Trophies.Remove(GOLD_TROPHY_ID);
+    }
+
+
+    private void CheckUnlockWave1Trophy(int waveNumber)
+    {
+        if (waveNumber < WAVE1) return;
+        Trophies.TryUnlock(WAVE1_TROPHY_ID);
+    }
+
+    private void CheckUnlockWave2Trophy(int waveNumber)
+    {
+        if (waveNumber < WAVE2) return;
+        Trophies.TryUnlock(WAVE2_TROPHY_ID);
+    }
+
+    private void CheckUnlockUnits1Trophy(int units)
+    {
+        if(units < UNITS1) return;
+        Trophies.TryUnlock(UNITS1_TROPHY_ID);
+    }
+
+    private void CheckUnlockUnits2Trophy(int units)
+    {
+        if (units < UNITS2) return;
+        Trophies.TryUnlock(UNITS2_TROPHY_ID);
+    }
+
+    private void CheckUnlockGoldTrophy(int gold)
+    {
+        if(gold < GOLD) return;
+        Trophies.TryUnlock(GOLD_TROPHY_ID);
+    }
+
+
+    private void EnemySpawnManager_OnWaveStartSpawn(object sender, EnemySpawnManager.OnWaveEventArgs e)
+    {
+        CheckUnlockWave1Trophy(e.waveNumber);
+        CheckUnlockWave2Trophy(e.waveNumber);
+    }
+
+    private void EntitiesManager_OnAnyAllyEntityAdded(object sender, EntitiesManager.OnEntityEventArgs e)
+    {
+        CheckUnlockUnits1Trophy(e.allyEntitiesCount);
+        CheckUnlockUnits2Trophy(e.allyEntitiesCount);
+    }
+
+    private void GoldManager_OnGoldIncreased(object sender, GoldManager.OnGoldEventArgs e)
+    {
+        CheckUnlockGoldTrophy(e.gold);
+    }
 }
